29716d399f45df2b66c5c9e690afd5236ca73aaa
Jose <pedrojcunha7@hotmail.com>
2024-10-11 15:57:32 +0200

    finish bank project

--- Program.cs
+++ Program.cs
@@ -1,17 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class User
+class Banque
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public decimal Balance { get; set; }
+    private List<Client> clients; // List of clients
+    private List<CompteBancaire> comptesBancaires; // List of bank accounts
 
-    public User(string firstName, string lastName, decimal initialBalance = 0)
+    public Banque()
     {
-        FirstName = firstName;
-        LastName = lastName;
-        Balance = initialBalance;
+        clients = new List<Client>();
+        comptesBancaires = new List<CompteBancaire>();
+    }
+
+    public Client EnregistrerClient(string prenom, string nom)
+    {
+        var client = new Client(prenom, nom);
+        if (!clients.Contains(client))
+        {
+            clients.Add(client);
+        }
+        return client;
+    }
+
+    public CompteBancaire CreerCompte(string prenom, string nom)
+    {
+        var client = EnregistrerClient(prenom, nom);
+        var compte = new CompteBancaire(client);
+        comptesBancaires.Add(compte);
+        return compte;
+    }
+
+    public void ObtenirResumeClient(Client client)
+    {
+        var comptesClient = comptesBancaires.FindAll(a => a.Proprietaire.Equals(client));
+        Console.WriteLine($"--------------\n{client.Prenom} {client.Nom} :");
+        decimal fortuneTotale = 0;
+        for (int i = 0; i < comptesClient.Count; i++)
+        {
+            Console.WriteLine($"- Compte #{comptesClient[i].IdCompte} : {comptesClient[i].Solde}");
+            fortuneTotale += comptesClient[i].Solde;
+        }
+        Console.WriteLine($"Fortune totale : {fortuneTotale}");
+    }
+
+    public CompteBancaire ObtenirCompteParId(int idCompte)
+    {
+        return comptesBancaires.Find(a => a.IdCompte == idCompte);
+    }
+
+    public List<CompteBancaire> ObtenirComptes(Client client)
+    {
+        return comptesBancaires.FindAll(a => a.Proprietaire.Equals(client));
+    }
+}
+
+class Client
+{
+    public string Prenom { get; private set; }
+    public string Nom { get; private set; }
+
+    public Client(string prenom, string nom)
+    {
+        Prenom = prenom;
+        Nom = nom;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Client client)
+        {
+            return Prenom == client.Prenom && Nom == client.Nom;
+        }
+        return false;
+    }
+}
+
+class CompteBancaire
+{
+    public int IdCompte { get; private set; }
+    public Client Proprietaire { get; private set; }
+    public decimal Solde { get; private set; }
+
+    private static int prochainIdCompte = 1;
+
+    public CompteBancaire(Client proprietaire)
+    {
+        IdCompte = prochainIdCompte++;
+        Proprietaire = proprietaire;
+        Solde = 0;
+    }
+
+    public bool Crediter(decimal montant)
+    {
+        if (montant <= 0) return false;
+        Solde += montant;
+        Console.WriteLine($"Compte #{IdCompte} de {Proprietaire.Prenom} {Proprietaire.Nom} : +{montant}");
+        return true;
+    }
+
+    public bool Debiter(decimal montant)
+    {
+        if (montant <= 0 || montant > Solde)
+        {
+            Console.WriteLine($"Compte #{IdCompte} de {Proprietaire.Prenom} {Proprietaire.Nom} : solde insuffisant (-{montant})");
+            return false;
+        }
+        Solde -= montant;
+        Console.WriteLine($"Compte #{IdCompte} de {Proprietaire.Prenom} {Proprietaire.Nom} : -{montant}");
+        return true;
+    }
+
+    public bool Transferer(decimal montant, CompteBancaire compteDestination)
+    {
+        if (Debiter(montant))
+        {
+            compteDestination.Crediter(montant);
+            Console.WriteLine($"Transfert du compte #{IdCompte} de {Proprietaire.Prenom} {Proprietaire.Nom} au compte #{compteDestination.IdCompte} de {compteDestination.Proprietaire.Prenom} {compteDestination.Proprietaire.Nom} réussi : {montant}");
+            return true;
+        }
+        Console.WriteLine($"Transfert du compte #{IdCompte} de {Proprietaire.Prenom} {Proprietaire.Nom} au compte #{compteDestination.IdCompte} de {compteDestination.Proprietaire.Prenom} {compteDestination.Proprietaire.Nom} échoué: solde insuffisant ({montant})");
+        return false;
     }
 }
 
@@ -19,50 +127,106 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<User> users = new List<User>
-        {
-            new User("Jose", "Gomes",900),
-            new User("Tatiana", "Isabel",1230),
-            new User("Paulo", "Cunha",5000)
-        };
+        Banque banque = new Banque();
 
-        Console.WriteLine("Hello, welcome to Statton Oakmont bank!");
-        Console.Write("Please, tell me your first name: ");
-        string firstName = Console.ReadLine();
-        
-        Console.Write("Please, tell me your last name: ");
-        string lastName = Console.ReadLine();
+        Console.WriteLine("Bienvenue à la banque!");
+        Console.Write("Entrez votre prénom: ");
+        string prenom = Console.ReadLine();
 
-        bool userExists = false;
-        foreach (var user in users)
-        {
-            if (user.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase) &&
-                user.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase))
-            {
-                userExists = true;
-                break;
-            }
-        }
+        Console.Write("Entrez votre nom: ");
+        string nom = Console.ReadLine();
 
-        if (userExists)
+        Client client = banque.EnregistrerClient(prenom, nom);
+
+        while (true)
         {
-            Console.WriteLine($"Welcome back {firstName} {lastName}!");
-            Console.WriteLine("Your balance is: {0:C}", users.Find(x => x.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase) && x.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase)).Balance);
-        }
-        else
-        {
-            Console.WriteLine("User not found.");
-            Console.WriteLine("Do you want to create a new account? (Y/N)");
-            string answer = Console.ReadLine();
-            
-            if (answer.Equals("Y", StringComparison.OrdinalIgnoreCase))
+            Console.WriteLine("\nChoisissez une option:");
+            Console.WriteLine("1. Créer un nouveau compte");
+            Console.WriteLine("2. Voir le solde de tous les comptes");
+            Console.WriteLine("3. Créditer un compte");
+            Console.WriteLine("4. Débiter un compte");
+            Console.WriteLine("5. Transférer de l'argent");
+            Console.WriteLine("6. Voir le résumé du client");
+            Console.WriteLine("7. Quitter");
+
+            string choix = Console.ReadLine();
+            switch (choix)
             {
-                users.Add(new User(firstName, lastName));
-                Console.WriteLine("Account created successfully!");
-            }
-            else
-            {
-                Console.WriteLine("Goodbye!");
+                case "1":
+                    banque.CreerCompte(prenom, nom);
+                    Console.WriteLine("Compte créé avec succès.");
+                    break;
+                case "2":
+                    var comptes = banque.ObtenirComptes(client);
+                    foreach (var compte in comptes)
+                    {
+                        Console.WriteLine($"Compte #{compte.IdCompte} : {compte.Solde}");
+                    }
+                    break;
+                case "3":
+                    Console.Write("Entrez l'ID du compte à créditer: ");
+                    int idCompteCredit = int.Parse(Console.ReadLine());
+                    var compteCredit = banque.ObtenirCompteParId(idCompteCredit);
+                    if (compteCredit != null)
+                    {
+                        Console.Write("Entrez le montant à créditer: ");
+                        decimal montantCredit = decimal.Parse(Console.ReadLine());
+                        compteCredit.Crediter(montantCredit);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Compte non trouvé.");
+                    }
+                    break;
+                case "4":
+                    Console.Write("Entrez l'ID du compte à débiter: ");
+                    int idCompteDebit = int.Parse(Console.ReadLine());
+                    var compteDebit = banque.ObtenirCompteParId(idCompteDebit);
+                    if (compteDebit != null)
+                    {
+                        Console.Write("Entrez le montant à débiter: ");
+                        decimal montantDebit = decimal.Parse(Console.ReadLine());
+                        compteDebit.Debiter(montantDebit);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Compte non trouvé.");
+                    }
+                    break;
+                case "5":
+                    Console.Write("Entrez l'ID du compte source: ");
+                    int idCompteSource = int.Parse(Console.ReadLine());
+                    var compteSource = banque.ObtenirCompteParId(idCompteSource);
+                    if (compteSource != null)
+                    {
+                        Console.Write("Entrez l'ID du compte de destination: ");
+                        int idCompteDestination = int.Parse(Console.ReadLine());
+                        var compteDestination = banque.ObtenirCompteParId(idCompteDestination);
+                        if (compteDestination != null)
+                        {
+                            Console.Write("Entrez le montant à transférer: ");
+                            decimal montantTransfert = decimal.Parse(Console.ReadLine());
+                            compteSource.Transferer(montantTransfert, compteDestination);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Compte de destination non trouvé.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Compte source non trouvé.");
+                    }
+                    break;
+                case "6":
+                    banque.ObtenirResumeClient(client);
+                    break;
+                case "7":
+                    Console.WriteLine("Merci d'avoir utilisé nos services. Au revoir!");
+                    return;
+                default:
+                    Console.WriteLine("Option invalide. Veuillez réessayer.");
+                    break;
             }
         }
     }
